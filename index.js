79be172b6a3d1ee851d861e2b15de7ef34619f71
Rakib <rakibpro98@gmail.com>
2023-06-02 21:19:08 +0600

    mongodb added and get method aded for appiontment and post method added for bookings

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +10,46 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@car-service-cluster.gmypetq.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Collections
+        const appointmentOptionCollection = client
+            .db('doctorsPortal')
+            .collection('appointmentOptions');
+        const bookingsCollection = client
+            .db('doctorsPortal')
+            .collection('booking');
+
+        // Appointment Data
+        app.get('/appointmentOptions', async (req, res) => {
+            const query = {};
+            const options = await appointmentOptionCollection
+                .find(query)
+                .toArray();
+            res.send(options);
+        });
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        });
+    } finally {
+    }
+}
+run().catch(console.dir);
 
 app.get('/', async (req, res) => {
     res.send('doctors portal server is running');
